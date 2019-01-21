7bcc070bd98ec63c0d2e7ccf084034799767314b
Judy Hu <judy.hu9@gmail.com>
2019-01-20 23:08:22 -0500

    controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,17 +25,18 @@ router.put("/api/burgers/:id", function (req, res) {
     console.log("condition", condition);
 
     burger.updateOne(
-        req.body.name,
-        req.body.devoured
-        , condition, function (result) {
-            if (result.changedRows == 0) {
-                // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
+        burger.updateOne(
+            req.body.name,
+            req.body.devoured
+            , condition, function (result) {
+                if (result.changedRows == 0) {
+                    // If no rows were changed, then the ID must not exist, so 404
+                    return res.status(404).end();
+                } else {
+                    res.status(200).end();
 
-            }
-        });
+                }
+            }));
 });
 
 
