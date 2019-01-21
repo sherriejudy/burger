a3a06537d58b993bda97a003658479ebbe9cae83
Judy Hu <judy.hu9@gmail.com>
2019-01-20 21:57:52 -0500

    fixed handlebars if unless

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
-        res.render("index", { burger_data: data })
+        res.render("index", { burger_data: data, notDevoured: true })
     });
 });
 
@@ -24,17 +24,18 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burger.updateOne({
-        name: req.body.name,
-        devoured: req.body.devoured
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+    burger.updateOne(
+        req.body.name,
+        req.body.devoured
+        , condition, function (result) {
+            if (result.changedRows == 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+
+            }
+        });
 });
 
 
