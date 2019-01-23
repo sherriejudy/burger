70a47306cf6322b201822eb544e3086d65e87601
Judy Hu <judy.hu9@gmail.com>
2019-01-22 21:46:39 -0500

    fixed redirect

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,10 +30,9 @@ router.put("/api/burgers/:id", function (req, res) {
         condition, req.params.id, function (result) {
             if (result.changedRows == 0) {
                 // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
+                return res.redirect("/");
             } else {
-                res.status(200).end();
-
+                res.redirect("/");
             }
         });
 });
