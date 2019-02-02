ecf3ff24b00e19dd9cf5f610b92db0aaa878d4aa
Judy Hu <judy.hu9@gmail.com>
2019-02-02 12:19:36 -0500

    JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,30 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
 });
 
-connection.connect(
-    function (err) {
-        if (err) {
-            console.log("There was an error " + err)
-        }
-        else {
-            console.log("Connected as id " + connection.threadId)
-        }
-    }
-);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "todoagain_db"
+  });
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.log("There was an error " + err);
+  } else {
+    console.log("Connected as id " + connection.threadId);
+  }
+});
 
 module.exports = connection;
 //require this inside orm
-
